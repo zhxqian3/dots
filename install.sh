af5d1ec7e2c6b0c4c61fddb5cc5e50a012e43281
zhxqian3 <140838276+zhxqian3@users.noreply.github.com>
2025-06-03 10:48:28 +0800

    rm unused pkg

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ pacman -S hyprpaper hypridle hyprlock swaybg
 pacman -S xdg-desktop-portal-gtk xdg-user-dirs pipewire wireplumber polkit-kde-agent
 pacman -S kitty waybar dunst wofi network-manager-applet wl-clipboard
 pacman -S pamixer playerctl pavucontrol pipewire-pulse helvum
-pacman -S gammastep brightnessctl blueman udiskie
+pacman -S gammastep brightnessctl udiskie
 pacman -S nwg-look qt5ct qt6ct breeze breeze5 breeze-gtk
 pacman -S qt5-wayland qt6-wayland
 
