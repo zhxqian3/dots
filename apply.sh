95ecaa8a47e875079094185019e3b3d96ca9f294
zhxqian3 <140838276+zhxqian3@users.noreply.github.com>
2025-06-06 11:52:56 +0800

    use ghostty as default terminal

--- apply.sh
+++ apply.sh
@@ -2,7 +2,7 @@
 rclone sync dunst/ ~/.config/dunst/ --verbose
 rclone sync gammastep/ ~/.config/gammastep/ --verbose
 rclone sync hypr/ ~/.config/hypr/ --verbose
-rclone sync kitty/ ~/.config/kitty/ --verbose
+rclone sync ghostty/ ~/.config/ghostty/ --verbose
 rclone sync waybar/ ~/.config/waybar/ --verbose
 rclone sync wofi/ ~/.config/wofi/ --verbose
 rclone sync niri/ ~/.config/niri/ --verbose
